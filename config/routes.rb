c226ab2d6643ebffe935f4a436239e7acc51e553
David le Roux <daveleroux@gmail.com>
2012-01-30 18:01:30 +0200

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Shalom::Application.routes.draw do
+  get "users/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +57,10 @@ Shalom::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  get "users/new"
+
+  match '/newuser',  :to => 'users#new'
+
+   root :to => 'pages#home'
 end
