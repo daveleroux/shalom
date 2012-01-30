44ff7bfbeab632ff725bad1cc929eb30fa551cf3
David le Roux <daveleroux@gmail.com>
2012-01-30 15:11:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shalom::Application.config.secret_token = '691c9fb55fa5c2fd1b6e6925bd6ed3ca99e05641adbe772ab772e93b6eb167a591e616c82fb570f92f3a32dd01d0b1ed7cbe0f36f654f2756ac2c1a30bdefe9f'
